b899f712c45025a47e37caab84d1d340b85494dd
eferati98 <ef69071@universum-ks.org>
2024-05-18 11:44:45 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+"""
+numri_1 = 6_000_000_000
+numri_2 = 5e305
+numri_3 = -5e400
+numri_4 = 5e-400
+
+print(numri_1 + 5)
+print(numri_2)
+print(numri_2)
+print(numri_3)
+print(numri_4)
+"""
+
+
+
+"""""
+print("Hello" * 5) "HelloHelloHelloHelloHello"
+print(2 ** 5)  "32"
+print(2 // 5) "0"
+"""""
+
+"""""
+nr_3 = 6
+nr_4 = 6
+
+print(nr_3 >= nr_4) #True
+print(nr_3 < nr_4) #False
+prnt(nr_3 != nr_4) #False
+print(6 == "6") # False #javascript me 2 barazime ish kan true, me 3 barazime ish kan false
+
+"""""
+"""""
+vlera_1 = True
+vlera_2 = False
+
+print(vlera_1 and vlera_2) # False
+print(vlera_1 or vlera_2) # True
+print(not vlera_1) #False
+"""
+
+#lageshtia = 125
+#print(lageshtia < 100)
+
+#mosha = 72
+#print(mosha >= 18 and mosha <= 65)
+
+
+
